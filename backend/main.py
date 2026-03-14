e682f16f7e841e05cc741fd8fb70b767481c03f2
Ramos <eddygonon84@gmail,com>
2026-03-14 07:09:38 -0600

    Corregir url del backend

--- backend/main.py
+++ backend/main.py
@@ -2,20 +2,23 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.routers import admin, auth, compiler, credentials
 
-app = FastAPI(title="UMG Basic Rover 1.0 API", version="1.0.0")
+app = FastAPI(title="UMG Basic Rover 2.0 API", version="1.2.0")
+
+origins = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "https://frontend-production-54ed.up.railway.app",
+]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:3000",
-        "http://127.0.0.1:3000",
-        "http://localhost:5173",
-        "http://127.0.0.1:5173",
-        "https://frontend-production-54ed.up.railway.app",
-    ],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"]
 )
 
 app.include_router(auth.router)
@@ -23,6 +26,10 @@ app.include_router(credentials.router)
 app.include_router(compiler.router)
 app.include_router(admin.router)
 
+
 @app.get("/")
 def root():
-    return {"message": "UMG Basic Rover 2.0 API funcionando", "version": "1.2.0"}
+    return {
+        "message": "UMG Basic Rover 2.0 API funcionando",
+        "version": "1.2.0"
+    }
